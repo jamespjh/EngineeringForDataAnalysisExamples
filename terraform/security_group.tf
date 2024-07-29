824be3d1fe7fe845e4d0664eb0b73a1bc96ab426
James Hetherington <j.hetherington@ucl.ac.uk>
2024-07-29 15:02:00 +0100

    Security group for intracluster communication

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -38,12 +38,18 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
 
-      # - proto: tcp
-      #   ports: 80
-      #   group_name: cluster_security_group
-      #   rule_desc: Allow each element of the cluster to access other machines in the cluster with WWW
-      #   # only works via PRIVATE IP
-      # - proto: tcp
-      #   ports: 22
-      #   group_name: cluster_security_group
-      #   rule_desc: Allow each element of the cluster to access other machines in the cluster with SSH
+resource "aws_vpc_security_group_ingress_rule" "allow_intracluster_ssh" {
+  security_group_id            = aws_security_group.course_group_terraform.id
+  referenced_security_group_id = aws_security_group.course_group_terraform.id
+  from_port                    = 22
+  ip_protocol                  = "tcp"
+  to_port                      = 22
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_intracluster_http" {
+  security_group_id            = aws_security_group.course_group_terraform.id
+  referenced_security_group_id = aws_security_group.course_group_terraform.id
+  from_port                    = 80
+  ip_protocol                  = "tcp"
+  to_port                      = 80
+}
