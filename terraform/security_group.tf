72838b513968ccc32c91d9f39299d9765ab17b3e
James Hetherington <j.hetherington@ucl.ac.uk>
2024-07-29 14:17:31 +0100

    Move cluster creation ansible version into subfolder

--- terraform/security_group.tf
+++ terraform/security_group.tf
@@ -37,3 +37,13 @@ resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv6" {
   cidr_ipv6         = "::/0"
   ip_protocol       = "-1" # semantically equivalent to all ports
 }
+
+      # - proto: tcp
+      #   ports: 80
+      #   group_name: cluster_security_group
+      #   rule_desc: Allow each element of the cluster to access other machines in the cluster with WWW
+      #   # only works via PRIVATE IP
+      # - proto: tcp
+      #   ports: 22
+      #   group_name: cluster_security_group
+      #   rule_desc: Allow each element of the cluster to access other machines in the cluster with SSH
